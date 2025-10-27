69fd6ae0f9da1d5b6b2219272c9201902bbf16ca
shift-primal <kasper@haugestol.com>
2025-10-27 11:16:39 +0100

    la til map på myrecipesview

--- geir_magi_mat/src/views/MyRecipesView.js
+++ geir_magi_mat/src/views/MyRecipesView.js
@@ -3,14 +3,13 @@ import { findRecipeByName } from "../utils/filter.js";
 import { MyRecipeCard } from "./components/MyRecipeCard.js";
 
 export function MyRecipesView() {
-    return /*HTML*/ `<div class="explore-recipes">
+	return /*HTML*/ `<div class="explore-recipes">
         <div class="hero-section">
             <h2>Mine oppskrifter</h2>
-                <div class="hero-section"></div>
-                <div class="recipeCard-group">${MyRecipeCard(findRecipeByName(model.recipes, "lasagne"))}</div>
-			<div class="recipeCard-group">${MyRecipeCard(findRecipeByName(model.recipes, "Kylling Tikka Masala"))}</div>
-			<div class="recipeCard-group">${MyRecipeCard(findRecipeByName(model.recipes, "Taco"))}</div>
-              
+                ${model.recipes.map((recipe) => {
+									return `
+                                                    <div class="recipeCard-group">${MyRecipeCard(recipe)}</div>`;
+								})}
         </div>
     </div>`;
 }
